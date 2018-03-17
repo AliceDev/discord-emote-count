e1e0554974cec83c5868da140c17399ee021682c
ZeLarpMaster <guillaumejean46@gmail.com>
2018-03-17 00:05:47 -0400

    Added the Cassandra implementation and a basic emote parser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Client = require("./lib/Client.js");
 const Background = require("./lib/Background.js");
+const Database = require("./lib/core/CassandraDatabase");
 
-let client = new Client();
+Database.checkTable();
+let client = new Client(Database);
 client.connect();
