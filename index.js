83ae3c25bddfbeaaef56586a16f18aa8b6c10510
ZeLarpMaster <guillaumejean46@gmail.com>
2018-04-02 19:04:35 -0400

    Fixes for the PR
    Namely:
    - Making EmoteParser's export an object to allow exporting other things later
    - Added a command line flag for the debugging logs (run with '-d' or '--debug')
    - Refactored the onReady event
    - Moved the topNEntries to config.json
    - Renamed graphify to emoteDataToGraph

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const commandLineArgs = {
+    DEBUGGING: ["--debug", "-d"],
+};
+
+// TODO: Make this generic when there will be too many
+process.env.DEBUGGING = process.argv.some(e => commandLineArgs.DEBUGGING.includes(e));
+
 const Client = require("./lib/Client.js");
 const Background = require("./lib/Background.js");
 const Database = require("./lib/core/CassandraDatabase");
