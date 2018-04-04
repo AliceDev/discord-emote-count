ff38ce9088d9c7ecb9a56270a023bdfedfb0a387
ZeLarpMaster <guillaumejean46@gmail.com>
2018-04-03 23:20:46 -0400

    Added colors; Added the background process (currently does nothing)

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const commandLineArgs = {
     DEBUGGING: ["--debug", "-d"],
+    COLORS: ["--colors"],
 };
 
 // TODO: Make this generic when there will be too many
 process.env.DEBUGGING = process.argv.some(e => commandLineArgs.DEBUGGING.includes(e));
+process.env.COLORS = process.argv.some(e => commandLineArgs.COLORS.includes(e));
 
 const Client = require("./lib/Client.js");
-const Background = require("./lib/Background.js");
+const child_process = require("child_process");
 const Database = require("./lib/core/CassandraDatabase");
+const auth = require("./config/auth.json");
+const { bashColors: { none, blue } } = require("./lib/core/Utils");
 
 Database.checkTable();
-let client = new Client(Database);
+const client = new Client(auth.token, Database);
 client.connect();
+
+const backgroundEnv = Object.assign({}, process.env, { BOT_TOKEN: auth.token });
+const backgroundProcess = child_process.fork("./lib/Background", [], { env: backgroundEnv });
+console.log("Background process PID:" + blue, backgroundProcess.pid, none);
