08959d29eb799f5cb06f02c454f89b6b57842ea0
ZeLarpMaster <guillaumejean46@gmail.com>
2018-04-09 00:51:00 -0400

    Completed the background process; Moved onMessageCreate and onMessageUpdate to the background process; Removed the count log

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const commandLineArgs = {
     DEBUGGING: ["--debug", "-d"],
-    COLORS: ["--colors"],
+    COLORS: ["--colors", "-c"],
 };
 
 // TODO: Make this generic when there will be too many
 process.env.DEBUGGING = process.argv.some(e => commandLineArgs.DEBUGGING.includes(e));
 process.env.COLORS = process.argv.some(e => commandLineArgs.COLORS.includes(e));
 
-const Client = require("./lib/Client.js");
-const child_process = require("child_process");
-const Database = require("./lib/core/CassandraDatabase");
 const auth = require("./config/auth.json");
 const { bashColors: { none, blue } } = require("./lib/core/Utils");
-
-Database.connect();
-const client = new Client(auth.token, Database);
-client.connect();
-
+const child_process = require("child_process");
 const backgroundEnv = Object.assign({}, process.env, { BOT_TOKEN: auth.token });
 const backgroundProcess = child_process.fork("./lib/Background", [], { env: backgroundEnv });
 console.log("Background process PID:" + blue, backgroundProcess.pid, none);
+
+const Client = require("./lib/Client.js");
+const Database = require("./lib/core/CassandraDatabase");
+Database.connect().then(() => {
+    const client = new Client(auth.token, Database);
+    client.connect();
+});
